4554ea3f6b106308c50fa249433f01f10bcc1911
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2022-07-20 08:36:57 -0400

    Added content to extra packages

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,13 +1,12 @@
 package Main;
 
 /**
- *
  * @author ICAZ117
  * @author Fishpaste
  * @author Maxwellington
  */
 public class Main {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
