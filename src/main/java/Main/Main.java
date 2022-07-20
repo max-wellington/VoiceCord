effd6cfc02b99f885f9ce98bb4beea822e3238b7
ICAZ117 <ibraheemcazalas@gmail.com>
2022-07-19 22:46:10 -0400

    Initialized Maven project with custom POM

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,14 @@
+package Main;
+
+/**
+ *
+ * @author ICAZ117
+ * @author Fishpaste
+ * @author Maxwellington
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
